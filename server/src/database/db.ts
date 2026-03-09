0d37afb46e4feb0a20e76acea214a8f13533bd03
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-03-08 23:34:01 -0400

    update the database connection to single string connectoin

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -8,11 +8,10 @@ const host = process.env.HOST as string;
 const port = process.env.PORT as string;
 const user = process.env.USER_NAME as string;
 const password = process.env.PASSWORD as string;
+
 const database = process.env.DATABASE_URL as string;
 
-export const sequelize = new Sequelize(database, user, password, {
-	host: host,
-	port: Number(port),
+export const sequelize = new Sequelize(database, {
 	dialect: 'postgres',
 	logging: false,
 	dialectOptions: {
